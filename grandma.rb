9fef3b8afcc925db733474ad43413cd53c59ed89
Ayana Bando <ayanaflax@yahoo.com>
2018-02-05 21:35:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(shout)
   if shout == 'I LOVE YOU GRANDMA'
-    return "I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN"
   elsif shout != shout.upcase
     'HUH?! SPEAK UP, SONNY!'
   else 'NO, NOT SINCE 1938'
-    
 end
